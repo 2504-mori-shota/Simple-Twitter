43dd5388ac778f845038d529bd2d4d96a4de019e
moir_shota <mori.shota.q@alhinc.jp>
2025-05-02 10:29:34 +0900

    first commit

--- src/chapter6/servise/UserService.java
+++ src/chapter6/servise/UserService.java
@@ -7,6 +7,8 @@ import java.sql.Connection;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.apache.commons.lang.StringUtils;
+
 import chapter6.beans.User;
 import chapter6.dao.UserDao;
 import chapter6.logging.InitApplication;
@@ -120,14 +122,29 @@ public class UserService {
 	        " : " + new Object(){}.getClass().getEnclosingMethod().getName());
 
 	        Connection connection = null;
-	        try {
-	            // パスワード暗号化
-	            String encPassword = CipherUtil.encrypt(user.getPassword());
-	            user.setPassword(encPassword);
 
-	            connection = getConnection();
-	            new UserDao().update(connection, user);
-	            commit(connection);
+	        ///これより下で条件分岐？→パスワードが入っていない場合をif文で分岐させる？☑
+
+	        ///null　-> isEmptyの書き換え方法わからない
+	        try {
+
+	        	String password = user.getPassword();
+
+	        	//[ !StringUtils.isEmpty(password))]が正しいかは判断できていない
+	        	//[ user.getPassword() != null]が変更前
+	        	if( !StringUtils.isEmpty(password)) {
+
+	        		 // パスワード暗号化
+	        		String encPassword = CipherUtil.encrypt(user.getPassword());
+	        		user.setPassword(encPassword);
+
+	        	}
+
+	        	connection = getConnection();
+	        	new UserDao().update(connection, user);
+	        	commit(connection);
+
+
 	        } catch (RuntimeException e) {
 	            rollback(connection);
 	    	  log.log(Level.SEVERE, new Object(){}.getClass().getEnclosingClass().getName() + " : " + e.toString(), e);

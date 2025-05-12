d7b85e01386e7c19aa9589792e19a62a931fafd0
moir_shota <mori.shota.q@alhinc.jp>
2025-05-12 17:44:40 +0900

    【レビュー再依頼】新規で作成したファイルをGitHubへCommit、Pushいたしました。 打鍵テスト1回目までのコードレビューをお願いいたします。

---
+++ src/chapter6/controller/EditServlet.java
@@ -0,0 +1,114 @@
+package chapter6.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.commons.lang.StringUtils;
+
+import chapter6.beans.Message;
+import chapter6.logging.InitApplication;
+import chapter6.servise.MessageService;
+
+@WebServlet(urlPatterns = { "/edit" })
+
+public class EditServlet extends HttpServlet {
+
+    /**
+    * ロガーインスタンスの生成
+    */
+    Logger log = Logger.getLogger("twitter");
+
+    /**
+    * デフォルトコンストラクタ
+    * アプリケーションの初期化を実施する。
+    */
+    public EditServlet() {
+        InitApplication application = InitApplication.getInstance();
+        application.init();
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+
+	  log.info(new Object(){}.getClass().getEnclosingClass().getName() +
+        " : " + new Object(){}.getClass().getEnclosingMethod().getName());
+
+	  HttpSession session = request.getSession();
+	  List<String> errorMessages = new ArrayList<String>();
+
+	  String strId = request.getParameter("id");
+
+	  if (!strId.matches("^[0-9]+$")) {
+    	  errorMessages.add("不正なパラメータが入力されました");
+          session.setAttribute("errorMessages", errorMessages);
+          response.sendRedirect("./");
+          return;
+	  }
+
+	  Message messageId = new MessageService().select(Integer.parseInt(strId));
+
+	  if (messageId == null) {
+		  errorMessages.add("不正なパラメータが入力されました");
+	      session.setAttribute("errorMessages", errorMessages);
+	      response.sendRedirect("./");
+	       return;
+	  }
+
+		  request.setAttribute("message", messageId);
+	      request.getRequestDispatcher("edit.jsp").forward(request, response);
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+
+	  log.info(new Object(){}.getClass().getEnclosingClass().getName() +
+        " : " + new Object(){}.getClass().getEnclosingMethod().getName());
+
+	  HttpSession session = request.getSession();
+      List<String> errorMessages = new ArrayList<String>();
+
+      String text = request.getParameter("text");
+      if (!isValid(text, errorMessages)) {
+          session.setAttribute("errorMessages", errorMessages);
+          response.sendRedirect("edit.jsp");
+          return;
+      }
+
+      Message message = new Message();
+      message.setId(Integer.parseInt(request.getParameter("id")));
+      message.setText(text);
+
+      new MessageService().update(message);
+      response.sendRedirect("./");
+  }
+
+  private boolean isValid(String text, List<String> errorMessages) {
+
+	  log.info(new Object(){}.getClass().getEnclosingClass().getName() +
+      " : " + new Object(){}.getClass().getEnclosingMethod().getName());
+
+      if (StringUtils.isBlank(text)) {
+          errorMessages.add("メッセージを入力してください");
+      } else if (140 < text.length()) {
+          errorMessages.add("140文字以下で入力してください");
+      }
+      if (errorMessages.size() != 0) {
+          return false;
+      }
+      return true;
+    }
+
+}
